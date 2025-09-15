561590d5b1b00858e1a13b88cc8973d08cd86261
BeguiledLeaf <joshthebob456@gmail.com>
2025-09-15 17:53:34 -0400

    Adding voltage reading & more comments

--- ky038.ino
+++ ky038.ino
@@ -3,9 +3,11 @@
  * 
  * Purpose: To collect sound data from the Microphone Sound Sensor Module KY-038
  * 
- * Notes: Turn potentiometer to change sensitivity 
+ * Notes: Turn potentiometer to change sensitivity. Left for less sensitivity, and vice versa. 
+          digital_out should be 1 when noise detected out 
+          analog values lower with more noise
  * 
- * Output format is: analog_out,digital_out
+ * Output format is: analog_out, analog_voltage, digital_out
  */
 
 #define analog A3 
@@ -22,6 +24,8 @@ void setup() {
 void loop() { 
   // Print out analog and digital readings from the KY-038
   Serial.print(analogRead(analog)); 
+  Serial.print(F(",")); 
+  Serial.print(analogRead(analog) * (5.0 / 1023.0));
   Serial.print(F(",")); 
   Serial.print(digitalRead(digital)); 
   Serial.print("\n");
