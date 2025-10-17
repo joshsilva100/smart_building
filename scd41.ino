a1edfa11901da1acfdc043f9eaa07fe7cb9b64de
joshsilva100 <joshthebob456@gmail.com>
2025-10-17 15:32:40 -0400

    Low power, only CO2

--- scd41.ino
+++ scd41.ino
@@ -2,9 +2,10 @@
  * HVAC/Smart Building
  * Joshua Silva
  *
- * Purpose: Initialize and collect CO2 data, Temperature, and Humidity from the Adafruit SCD-41 CO2 sensor
+ * Purpose: Initialize and collect CO2 data from the Adafruit SCD-41 CO2 sensor 
+ *          Don't need the extra Temperature and Humidity data
  * 
- * Output format is CO2_Concentration,Temperature,Humidity 
+ * Output format is CO2_Concentration
  * 
  * More info: https://github.com/Sensirion/arduino-i2c-scd4x/tree/master/src
  */ 
@@ -16,26 +17,29 @@ SensirionI2cScd4x scd41;
 
 void setup() {
 
-    Serial.begin(115200);
+    Serial.begin(115200); 
     Wire.begin();
     scd41.begin(Wire, SCD41_I2C_ADDR_62);
-    scd41.wakeUp();
-    scd41.startPeriodicMeasurement();
+    scd41.wakeUp(); 
+    //scd41.startPeriodicMeasurement();
+    scd41.startLowPowerPeriodicMeasurement(); // Use low power in preparation of complete prototype
 }
 
 void loop() {
     uint16_t co2_ppm = 0;
     float temp = 0.0;
     float humid = 0.0;
-    
-    scd41.readMeasurement(co2_ppm, temp, humid);
+
+    // Raw works fine as well if we don't care about extra checks & float conversion
+    scd41.readMeasurement(co2_ppm, temp, humid); 
     
     Serial.print(co2_ppm);
-    Serial.print(F(","));
-    Serial.print(temp);
-    Serial.print(F(","));
-    Serial.print(humid);
+    // Serial.print(F(","));
+    // Serial.print(temp);
+    // Serial.print(F(","));
+    // Serial.print(humid);
     Serial.print(F("\n")); 
 
-    delay(5000);
+    //delay(5000); 
+    delay(30000); // With low power, takes 25 seconds longer to get next reading
 }
