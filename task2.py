4055773b20eeddedfc24f0825667fb825b265ff3
gingerc2402 <122637566+gingerc2402@users.noreply.github.com>
2025-11-03 21:46:52 -0500

    30 min intervals, Seperated data files

--- task2.py
+++ task2.py
@@ -8,7 +8,11 @@ import glob
 import sys
 import datetime
 import schedule
+import socket
+import subprocess
 
+
+SIZE = 4096 # hard coded size, i don't trust it to make major issues when transporting a small string 
 
 def read_data(serialcomm):
     data = serialcomm.readline().decode('ascii')
@@ -19,10 +23,46 @@ def read_data(serialcomm):
     #print(data) #print the line to ensure correctnesss
     return data
 
-def next_hour(): # find next hour
-    delta = datetime.timedelta(hours=1)
+
+def start_hotspot_IP(hotspot, PORT):
+    if (int(hotspot) == 1): 
+        subprocess.run(["nmcli","dev","wifi","hotspot","ifname","wlan0","ssid","HVACR","password","INNO4990~"])
+    subprocess.run(["hostname","-I"])
+    subprocess.run(["sudo","ufw","allow", str(PORT)])
+
+
+
+def read_data_wireless(HOST, PORT, SIZE):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: 
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((HOST,PORT))
+        
+        s.listen()
+        conn, addr = s.accept()
+        data = 0
+        with conn: 
+            print(f"Connected at {addr}")
+            while not data:
+                data = conn.recv(SIZE)
+                #time.sleep(.1); 
+                #if not data:
+                #   break
+                print(f"Received: {data.decode()}")
+            
+        
+        return data.decode()
+        
+
+
+
+def next_hour(): # find next hour or a half
     cur2 = datetime.datetime.now()
-    next = (cur2 + delta).replace(microsecond=0, second=0, minute=0)
+    if(cur2.minute < 30):
+        #delta = datetime.timedelta(minutes=30)
+        next = (cur2).replace(microsecond=0, second=0, minute=30)
+    else:
+        delta = datetime.timedelta(hours=1)
+        next = (cur2 + delta).replace(microsecond=0, second=0, minute=0)
     return next
 
 
@@ -37,15 +77,28 @@ def hour_break():
     time.sleep(totalsecs)
 
 
-def interval(serialname):
+def interval(): #serialname):
     now = datetime.datetime.now()
     end_time = now + datetime.timedelta(minutes=5.1) 
-
-    file_name = f"data/{now.year}-{now.month}-{now.day}_{now.hour}.txt"
-    with open(file_name,"a") as file: 
-        file.write(f"Timestamp,AHT20_Temp_C,AHT20_Hum_%,DHT11_Temp_C,DHT11_Hum_%\n")
-        schedule.every(29.97).seconds.do(thejob, file=file, serialname=serialname)
-        thejob(file, serialname)
+    # seperate files now specified
+    types = ["temp", "humid", "co2", "lux", "noise"]
+    file_names = []
+    for types in types: 
+        file_names.append(f"data/{types}_{now.year}-{now.month}-{now.day}_{now.hour}.txt")
+    #file_name = f"data/{now.year}-{now.month}-{now.day}_{now.hour}.txt"
+    with open(file_names[0],"a") as file0,\
+    open(file_names[1],"a") as file1,\
+    open(file_names[2],"a") as file2,\
+    open(file_names[3],"a") as file3,\
+    open(file_names[4],"a") as file4: 
+        file0.write(f"Timestamp,Temp\n")
+        file1.write(f"Timestamp,Humid\n")
+        file2.write(f"Timestamp,C02%\n")
+        file3.write(f"Timestamp,Lux%\n")
+        file4.write(f"Timestamp,Noise%\n")
+        files = [file0,file1,file2,file3,file4]
+        schedule.every(29.97).seconds.do(thejob, files=files)#, serialname=serialname)
+        thejob(files)#, serialname)
         while datetime.datetime.now() < end_time:
             schedule.run_pending()
             time.sleep(.1)
@@ -53,17 +106,21 @@ def interval(serialname):
         schedule.clear()
     
 
-def thejob(file, serialname): #the job to occur every 30 seconds in this case the getting of temp humid and time and writing and flushing
-    data=read_data(serialname)
-    
+def thejob(files):#, serialname): #the job to occur every 30 seconds in this case the getting of temp humid and time and writing and flushing
+    #data=read_data(serialname)
+    data = read_data_wireless("10.42.0.1",5400,4096)
+    #types = ["temp", "humid", "co2", "lux", "noise"]
+    data_types = [temp,humid,co2,lux,noise] = data.split(',')
     now = datetime.datetime.now() 
     # set rounding line instead of truncating, has been done
     if now.microsecond > 500000: 
         now = now + datetime.timedelta(seconds=1)
     
+
     now = now.replace(microsecond=0)
-    file.write(f"{now},{data}")
-    file.flush()
+    for i in range(5):
+        files[i].write(f"{now},{float(data_types[i]):.5f}\n")
+        files[i].flush()
     print ("Interval Started at: ", datetime.datetime.now(), "and printed: ", now)
 
 def serial_ports(): # finds all serial ports connected to the computer and puts them in a list of portnames
@@ -94,29 +151,38 @@ def serial_ports(): # finds all serial ports connected to the computer and puts 
             pass
     return result
 
+
+
 #start code here
 
 
-print("Hey Dr. Guo, Let's go gambling!")
+#print("Hey Dr. Guo, Let's go gambling!")
+hotspot = input("Turn hotspot on? (1 for yes or 0 for no):")
+HOST = "10.42.0.1" 
+PORT = input("Enter the Port number (1-39999): ")
+start_hotspot_IP(hotspot,PORT)
+#HOST = input("Enter the IP shown above: ")
+input("Press Enter once ESP is connected: ")
 
-name_serial_port = serial_ports()
-print("The following serial port(s) detected: ", name_serial_port) # testing and to help user input correctly
-print("Current number of Serial ports: ", len(name_serial_port))
+
+#name_serial_port = serial_ports()
+#print("The following serial port(s) detected: ", name_serial_port) # testing and to help user input correctly
+#print("Current number of Serial ports: ", len(name_serial_port))
 
 
-if len(name_serial_port) == 1: #finds the port name and if only one serial port is detected puts it in for you 
-    print("Portname for the one port: ", name_serial_port[0])
-    serialcomm = serial.Serial(port=name_serial_port[0],baudrate=9600,timeout=1) # Replace portname here as fit 
-else :
-    user_input = input("Error detecting port\nEnter the Arduino Port: ") # if port not detected or too many copy one that should be shown or find it and input will make better
-    serialcomm = serial.Serial(port=user_input,baudrate=9600,timeout=1) 
-time.sleep(2)
+#if len(name_serial_port) == 1: #finds the port name and if only one serial port is detected puts it in for you 
+#    print("Portname for the one port: ", name_serial_port[0])
+#    serialcomm = serial.Serial(port=name_serial_port[0],baudrate=9600,timeout=1) # Replace portname here as fit 
+#else :
+#    user_input = input("Error detecting port\nEnter the Arduino Port: ") # if port not detected or too many copy one that should be shown or find it and input will make better
+#    serialcomm = serial.Serial(port=user_input,baudrate=9600,timeout=1) 
+#time.sleep(2)
 
 schedule.clear()
 
 print("Started at: ", datetime.datetime.now()) # for testing 
-interval(serialcomm)
+interval()#serialcomm)
 while True:
     hour_break()
     print ("Break ended at: ", datetime.datetime.now()) # for testing
-    interval(serialcomm) 
+    interval()#serialcomm) 
